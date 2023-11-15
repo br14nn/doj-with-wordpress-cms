ad21849f3a71ebe97ed39a4bc5cf49836ca3c85e
br14nn <vituallabrian727@gmail.com>
2023-11-15 22:59:02 +0800

    fixing build issues #3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,34 @@ import fetchRecentNews from "@/utils/fetchers/fetchRecentNews";
 export default async function Home() {
   const recentNewsData: { data: any[]; ok: boolean } = await fetchRecentNews();
 
+  const renderRecentNews = async () => {
+    if (recentNewsData !== null) {
+      return recentNewsData.data.map((news) => (
+        <CustomNewsCard
+          key={nanoid()}
+          thumbnailWidth={
+            news._embedded["wp:featuredmedia"][0]["media_details"]["sizes"][
+              "full"
+            ].width
+          }
+          thumbnailHeight={
+            news._embedded["wp:featuredmedia"][0]["media_details"]["sizes"][
+              "full"
+            ].height
+          }
+          thumbnailImage={
+            news._embedded["wp:featuredmedia"][0]["media_details"]["sizes"][
+              "full"
+            ].source_url
+          }
+          thumbnailAlt={news["_embedded"]["wp:featuredmedia"][0].alt_text}
+        >
+          {news["title"]["rendered"].toUpperCase()}
+        </CustomNewsCard>
+      ));
+    }
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center pt-[70px]">
       <ImageSlider />
@@ -21,29 +49,7 @@ export default async function Home() {
         </CustomHeading1>
 
         <div className="grid grid-cols-1 place-items-center gap-16 md:grid-cols-2 2xl:grid-cols-3">
-          {recentNewsData.data.map((news) => (
-            <CustomNewsCard
-              key={nanoid()}
-              thumbnailWidth={
-                news._embedded["wp:featuredmedia"][0]["media_details"]["sizes"][
-                  "full"
-                ].width
-              }
-              thumbnailHeight={
-                news._embedded["wp:featuredmedia"][0]["media_details"]["sizes"][
-                  "full"
-                ].height
-              }
-              thumbnailImage={
-                news._embedded["wp:featuredmedia"][0]["media_details"]["sizes"][
-                  "full"
-                ].source_url
-              }
-              thumbnailAlt={news["_embedded"]["wp:featuredmedia"][0].alt_text}
-            >
-              {news["title"]["rendered"].toUpperCase()}
-            </CustomNewsCard>
-          ))}
+          {renderRecentNews()}
         </div>
       </section>
     </main>

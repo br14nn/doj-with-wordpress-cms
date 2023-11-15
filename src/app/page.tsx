9bbcd7c4a029b4436c8d58e0d00ed8a6e8605a72
br14nn <vituallabrian727@gmail.com>
2023-11-15 17:52:30 +0800

    created CustomHeading1 component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import CustomHeading1 from "@/components/CustomHeading1/CustomHeading1";
 import ImageSlider from "@/components/ImageSlider/ImageSlider";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between pt-[70px]">
+    <main className="flex min-h-screen flex-col items-center pt-[70px]">
       <ImageSlider />
+      <section className="p-16" id="newsSection">
+        <CustomHeading1>RECENT NEWS</CustomHeading1>
+      </section>
     </main>
   );
 }
--- src/components/ImageSlider/ImageSlide.tsx
+++ src/components/ImageSlider/ImageSlide.tsx
@@ -4,7 +4,7 @@ interface IImageSlideProps {
 
 export default function ImageSlide(props: IImageSlideProps) {
   return (
-    <div className="h-[400px] w-full bg-red-500 3xl:h-[800px]">
+    <div className="h-[400px] w-full bg-gray-500 3xl:h-[800px]">
       {props.children}
     </div>
   );

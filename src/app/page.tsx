1d4f807b5651377ab020a01358776ee0b9b0c2c6
br14nn <vituallabrian727@gmail.com>
2023-11-15 23:24:43 +0800

    fixing build issues #6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,14 @@
-import { nanoid } from "nanoid";
-
 import CustomHeading1 from "@/components/CustomHeading1/CustomHeading1";
-import CustomNewsCard from "@/components/CustomNewsCard/CustomNewsCard";
 import ImageSlider from "@/components/ImageSlider/ImageSlider";
-import fetchRecentNews from "@/libs/fetchers/fetchRecentNews";
+import RecentNews from "@/components/RecentNews/RecentNews";
 
-export default async function Home() {
-  const recentNewsData: { data: any[]; ok: boolean } = await fetchRecentNews();
-
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center pt-[70px]">
       <ImageSlider />
 
       <section
-        className="3xl:p-32 flex flex-col gap-16 p-16"
+        className="flex flex-col gap-16 p-16 3xl:p-32"
         id="recentNewsSection"
       >
         <CustomHeading1 underlineColor="border-apache">
@@ -21,30 +16,7 @@ export default async function Home() {
         </CustomHeading1>
 
         <div className="grid grid-cols-1 place-items-center gap-16 md:grid-cols-2 2xl:grid-cols-3">
-          {recentNewsData.data !== null &&
-            recentNewsData.data.map((news) => (
-              <CustomNewsCard
-                key={nanoid()}
-                thumbnailWidth={
-                  news._embedded["wp:featuredmedia"][0]["media_details"][
-                    "sizes"
-                  ]["full"].width
-                }
-                thumbnailHeight={
-                  news._embedded["wp:featuredmedia"][0]["media_details"][
-                    "sizes"
-                  ]["full"].height
-                }
-                thumbnailImage={
-                  news._embedded["wp:featuredmedia"][0]["media_details"][
-                    "sizes"
-                  ]["full"].source_url
-                }
-                thumbnailAlt={news["_embedded"]["wp:featuredmedia"][0].alt_text}
-              >
-                {news["title"]["rendered"].toUpperCase()}
-              </CustomNewsCard>
-            ))}
+          <RecentNews />
         </div>
       </section>
     </main>

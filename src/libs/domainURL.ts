04f835835fb3cc3e8fa61465dc8319393cb60585
br14nn <vituallabrian727@gmail.com>
2023-11-16 00:06:58 +0800

    fixing build issues #12

--- src/libs/domainURL.ts
+++ src/libs/domainURL.ts
@@ -1,3 +1,9 @@
-const domainURL = "https://new-doj-website.vercel.app";
+//http://localhost:3000
+//https://new-doj-website.vercel.app
+
+const local = "http://localhost:3000";
+const official = "https://new-doj-website.vercel.app";
+
+const domainURL = official;
 
 export default domainURL;
